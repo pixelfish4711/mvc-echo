b05665b37961330cf05a1e9bc367e84bca5ce68f
Boris Ruch <borisruch@gmail.com>
2018-06-02 18:26:09 +0200

    delay 1000ms

--- src/test/java/com/boo/mvcecho/EndpointTest.java
+++ src/test/java/com/boo/mvcecho/EndpointTest.java
@@ -2,7 +2,7 @@ package com.boo.mvcecho;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
